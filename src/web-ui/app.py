c8425176df4a1a4be377c8bfd53380276c104cf0
gr3edydevel0per <kanav.105056@stu.upes.ac.in>
2025-09-20 01:38:06 +0530

    Login and Sign Template Added

--- src/web-ui/app.py
+++ src/web-ui/app.py
@@ -11,4 +11,14 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+
+@app.get("/dashboard", response_class=HTMLResponse)
+async def read_root(request: Request):
     return templates.TemplateResponse("dashboard.html", {"request": request})
+
+
+@app.get("/2fa", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("2fa.html", {"request": request})
